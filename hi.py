eb6ebaa471cb7c1456400da377ae371a6154f889
adamuqhris-a11y <adamuqhris@gmail.com>
2026-03-10 11:46:37 +0800

    Refactor login system and update logo URL

--- hi.py
+++ hi.py
@@ -13,8 +13,8 @@ import os
 # 1. KONFIGURASI HALAMAN
 st.set_page_config(page_title="PUO Geomatics Pro", layout="wide")
 
-# URL Logo (Gunakan URL baru atau path fail yang anda muat naik)
-LOGO_URL = "https://i.ibb.co/LzXpYyM/politeknik-ungku-omar-seeklogo-removebg-preview-png" 
+# URL Logo Rasmi PUO
+LOGO_URL = "https://i.ibb.co/LzXpYyM/politeknik-ungku-omar-seeklogo-removebg-preview-png.png" 
 
 # 2. SISTEM LOGIN
 def get_stored_password():
@@ -49,23 +49,4 @@ def auth_interface():
         with st.form("login_form"):
             u_id = st.text_input("ID Pengguna")
             u_pw = st.text_input("Kata Laluan", type="password")
-            submit = st.form_submit_button("Masuk", use_container_width=True)
-            if submit:
-                st.session_state["user_db"] = load_users()
-                if u_id in st.session_state["user_db"] and st.session_state["user_db"][u_id] == u_pw:
-                    st.session_state["logged_in"] = True
-                    st.session_state["current_user"] = u_id
-                    st.rerun()
-                else: 
-                    st.error("ID atau Kata Laluan salah!")
-        with st.expander("Tukar Kata Laluan Baru"):
-            with st.form("change_pw_form"):
-                new_pw = st.text_input("Masukkan Kata Laluan Baru", type="password")
-                confirm_pw = st.text_input("Sahkan Kata Laluan Baru", type="password")
-                change_btn = st.form_submit_button("Kemaskini Kata Laluan")
-                if change_btn:
-                    if new_pw == confirm_pw and new_pw != "":
-                        save_password(new_pw)
-                        st.session_state["user_db"] = load_users()
-                        st.success(f
-
+            submit = st.form_submit_button
